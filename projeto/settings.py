07fdc25d0ff2f898e7e5833727ec482f5b48468e
israel Bezerra <israelwerther48@outlook.com>
2020-12-26 01:29:12 -0300

    criando server de backup

--- projeto/settings.py
+++ projeto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'israeltestes2.herokuapp.com', 'esctop.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'esctopbackup.herokuapp.com', 'esctop.herokuapp.com']
 
 # Application definition
 
