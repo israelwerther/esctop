dea31557aca1172467dda112ca67fcf9f2e7ce87
israel Bezerra <israelwerther48@outlook.com>
2021-01-11 23:58:34 -0300

    comentei e limpei comentarios do cliente cnpj

--- projeto/settings.py
+++ projeto/settings.py
@@ -27,7 +27,14 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'israeltestes2.herokuapp.com', 'esctop.herokuapp.com', 'escback1.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    'israeltestes2.herokuapp.com', 
+    'esctop.herokuapp.com', 
+    'escback1.herokuapp.com', 
+    'escback2.herokuapp.com'
+]
 
 # Application definition
 
