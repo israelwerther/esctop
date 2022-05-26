b187fe83e8762e1b433b42115be23bd21e02431f
israelwerther <israelwerther48@outlook.com>
2022-05-26 08:27:59 -0300

    testes+

--- projeto/emprestimo/admin.py
+++ projeto/emprestimo/admin.py
@@ -6,7 +6,7 @@ class EmprestimoAdmin(admin.ModelAdmin):
     list_display=(        
         'n_contrato', 'valor_multa','juros_ao_dia', 'valor_mutuado', 'juros_ao_mes', 'valor_emprestado', 'created_at',        
     )
-    search_fields=('n_contrato',)
+    search_fields=('n_contrato', 'cliente', 'cliente_cnpj')
     # readonly_fields = ["valor_prestacao", ]
     # list_filter=('rg',)   
     ordering = ('-dt_emprestimo',)
